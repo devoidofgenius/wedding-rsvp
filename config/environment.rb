c351e3634f8193660913e84ce3564eb9ed1c484d
Chris Ellinger <cellinger@gmail.com>
2015-03-19 18:48:35 -0400

    Configure mailer settings and add Figaro

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['app35043956@heroku.com'],
-      :password       => ENV['Br0k3nh@ck0rz'],
-      :domain         => 'heroku.com',
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'gracemika-wedding.herokuapp.com',
       :enable_starttls_auto => true
 }
